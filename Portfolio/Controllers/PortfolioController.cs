6cddd6fac66625854c3e6929a0983992f82b8d45
Your Name <you@example.com>
2016-05-26 11:00:18 -0400

    Setting repo

---
+++ Portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioEntities db = new PortfolioEntities();
+        // GET: Portfolio
+        public ActionResult Index()
+        {
+            return View(db.Projects);
+        }
+    }
+}
